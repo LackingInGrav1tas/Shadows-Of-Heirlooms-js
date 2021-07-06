5b4a40647566f445006172bec847d224cc9232e4
LackingInGrav1tas <42680395+LackingInGrav1tas@users.noreply.github.com>
2021-07-06 00:48:09 -0400

    fixed

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
 class StoryFork {
     constructor(description, choice_desc, canvas_fun, options) {
-        // str, str, [str, ...]
+        // str, str, function, [str, ...]
         this.description = description;
         this.choice_desc = choice_desc;
         this.canvas_fun = canvas_fun;
         this.options = options;
     }
     render() {
+        // setting description
         document.getElementById('description').textContent = this.description;
+
         for (var i = 1; i < this.options.length + 1; i++) {
+            // showing the options
             let elem = document.getElementById('choice' + i);
             elem.style.display = 'block';
             elem.textContent = this.options[i-1].choice_desc;
         }
         for (; i < 8; i++) {
+            // hiding the rest
             document.getElementById('choice' + i).style.display = 'none';
         }
         this.canvas_fun();
@@ -22,6 +26,7 @@ class StoryFork {
 
 class Game {
     constructor() {
+        // the entire game
         this.current = new StoryFork('START', 'not needed b/c first', function(){}, [
             new StoryFork('1', '1111111111111111', function(){}, [
                 new StoryFork('11', '1111111111111111', function(){}, [
@@ -40,17 +45,21 @@ class Game {
                 ]),
             ]),
         ]);
+
+        // stores user history (string form)
         this.history = [];
     }
 
     selectPath(index) {
+        // selects option
         this.current = this.current.options[index-1];
         this.current.render();
     }
 
     goto(from) {
+        // sets current value from string form ('13221' = 1st option, 3rd option, etc.)
         for (var i = 0; i < from.length; i++) {
-            this.current = this.current.options[ Number(from[i]) + 1 ];
+            this.current = this.current.options[ Number(from[i]) - 1 ];
         }
     }
 
@@ -61,19 +70,6 @@ class Game {
 }
 
 var game = new Game();
-
-/*// hides/shows options
-function toggleOptions() {
-    let options = ['choice1', 'choice2', 'choice3', 'choice4', 'choice5', 'choice6', 'choice7'];
-    for (var i = 0; i < options.length; i++) {
-        let elem = document.getElementById(options[i]);
-        if (elem.style.visibility == 'hidden') {
-            elem.style.visibility = 'visible';
-        } else {
-            elem.style.visibility = 'hidden';
-        }
-    }
-}*/
 
 // changes font color of element
 function changeColor(id, color) {
